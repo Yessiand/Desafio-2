17195547db5776c7a3bdbace65ef6a12bcd0bd79
Yessiand <aportillacu@gmail.com>
2022-09-12 21:36:04 -0500

    Agregando arrays al proyecto

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 /*REGISTRO PARA EL TEST*/
 function registro() {
-    let opciones = Number(prompt(`Si desea registrarse ingrese 1 \n Si ya se encuentra registrado ingrese 2 \n Si desea no registrarse y seguir como invitado ingrese 3`))
+    let opciones = Number(prompt(`Si desea registrarse ingrese 1 
+    \nSi ya se encuentra registrado ingrese 2 
+    \nSi desea no registrarse y seguir como invitado ingrese 3`));
 
     switch(opciones) {
         case 1 : ingreso(); break;
@@ -20,13 +22,12 @@ function ingreso(user, clave, repclave, email) {
     user = prompt("Ingrese su nombre de usuario");
     clave = prompt("Ingrese su clave");
     repclave = prompt("Repita la clave");
-    if(repclave != clave){
+    while(repclave != clave){
         alert("Las claves no coinciden");
         alert("Vuelva a escribir la clave");
         repclave = prompt("Vuelve a ingresar la clave");
     }    
     email = prompt("Ingrese su email");
-
     alert(`Puedes continuar para comenzar el test, ${user} `);
 }
 
@@ -40,21 +41,38 @@ function logeo(user, clave) {
 }
 
 /*VARIABLES SIMULADOR*/
-let respuesta = "TRIANGULACION";
+const miArray = ["TRIANGULACION", "SISTEMA FAMILIAR", "CONFLICTO FAMILIAR"];
+let respuesta = miArray[0] ;
 let acierto = false;
 let turnos = 4;
 let mensaje = "";
 
 /*SIMULADOR INTERACTIVO DE PREGUNTAS*/
+let contador= 0;
+
+
 while (turnos > 0  && acierto == false) {
-    let respUsuario = prompt ("Se define como un patron relacional entre tres elementos de un sistema. El triángulo clásico es el formado por el hijo, el padre y la madre, en el que dos están unidos frente a un tercero que está incómodo en la situación.")
-    if (respUsuario.toUpperCase() == respuesta) {
-        acierto=true; 
-        turnos--;
-        alert ("Felicitaciones, has acertado");
+    let respUsuario = prompt (
+`Se define como un patron relacional entre tres elementos de un sistema.
+El triángulo clásico es el formado por el hijo, el padre y la madre, en el que dos están unidos frente a un tercero que está incómodo en la situación. 
+Escriba la respuesta correcta: 
+- ${miArray[0]}
+- ${miArray[1]}
+- ${miArray[2]}
+`);
+
+    if (miArray.indexOf(respUsuario.toUpperCase()) != -1) {
+        if (respUsuario.toUpperCase() == respuesta) {
+            acierto=true; 
+            turnos--;
+            alert ("Felicitaciones, has acertado");
+        } else {
+            turnos--;
+            alert(`Vuelve a intentarlo, te quedan  ${turnos} intentos`)
+        }
     } else {
         turnos--;
-        alert(`Vuelve a intentarlo, te quedan " + ${turnos} + " intentos`)
+        alert(`La respuesta ingresada no pertenece a las opciones validas. Vuelve a intentarlo, te quedan ${turnos} intentos.`)
     }
 }
 if (acierto == true) {
@@ -63,3 +81,5 @@ if (acierto == true) {
     mensaje = "Usted perdio.";
 }
 alert("El juego termino, " + mensaje);
+
+
